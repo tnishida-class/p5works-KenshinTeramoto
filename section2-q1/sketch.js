4ae95260a055800a9a80d3ee90a7df3c426847e6
KenshinTeramoto <kenshin-teramoto@outlook.jp>
2020-10-20 15:24:48 +0900

    Change section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,16 @@
 // 小手調べ
 function setup(){
+  createCanvas(200, 200);
+  fill(0);
   for(let i = 0; i < 10; i++){
     // BLANK[1]
+    noFill();
+   if(i < 6){
+     stroke(0, 0, 255);
+   } else{
+     stroke(255, 0, 0);
+   }
+   let s = i *10 + 1;
+   ellipse(50, 50, s);
+    }
   }
-}
