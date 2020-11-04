ea62382b61131545fa8772524f5bbbab1b496e52
KenshinTeramoto <kenshin-teramoto@outlook.jp>
2020-11-04 17:43:00 +0900

    Change section3-2

--- section3-2/sketch.js
+++ section3-2/sketch.js
@@ -27,7 +27,7 @@ function ngmark(cx, cy, r){
 function star(cx, cy, r){
   beginShape();
   for(var i = 0; i < 5; i++){
-    let theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    let theta = TWO_PI * i * 1 / 5 - HALF_PI;
     let x = cx + cos(theta) * r;
     let y = cy + sin(theta) * r;
     vertex(x,y);
