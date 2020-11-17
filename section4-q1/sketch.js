0c4339cdcdd81e1c6acbf24d1161d99c2691c734
KenshinTeramoto <kenshin-teramoto@outlook.jp>
2020-11-17 12:03:44 +0900

    再提出　section4-q1

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -12,9 +12,12 @@ function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
   // BLANK[1]
-let size = 100;
+let size;
 if(count < 50){
-  size = 200;
+  size =100 -count;
+}else{
+  size = count;
+
 }
   ellipse(width / 2, height / 2, size);
 }
