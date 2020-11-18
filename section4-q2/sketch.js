ac38fe4519f86bc803ae47470456cd7cda10db23
KenshinTeramoto <kenshin-teramoto@outlook.jp>
2020-11-18 16:28:25 +0900

    Update sketch.js
    Change section4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -1,17 +1,26 @@
 // テキスト「アニメーション」
-let x, y, vx, vy;
+let x, y, vx, vy, angle;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
   y = height / 2;
-  vx = 8;
-  vy = 8;
+  vx = 2;
+  vy = 2;
+  angle = 0;
 }
 
+
 function draw(){
-  background(160, 192, 255);
+  background(50, 55, 100);
   // BLANK[2] (hint: 作った star 関数を使います)
+noStroke();
+fill('#0f0');
+  star(x, y, 100, angle);
+  x += vx;
+  y -= vy;
+  angle += 10;
+
 
   // 端の処理パターン (1) 反対側から出てくる
   if(x > width){ x = 0; }
@@ -25,6 +34,7 @@ function star(cx, cy, r, angle){
   for(var i = 0; i < 20; i++){
     var theta = TWO_PI * i * 2 / 5 - HALF_PI;
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
+    theta += angle;
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
     vertex(x,y);
